04734cd9cb7c4df7761071aa50af37d648253636
Zhenishai <113972797+Zhenishai@users.noreply.github.com>
2024-06-27 10:52:13 +0200

    Update ListadoDeEventos.jsx

--- src/components/ListadoDeEventos.jsx
+++ src/components/ListadoDeEventos.jsx
@@ -1,16 +1,60 @@
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import { getItems, getUsersByUserId } from "../app/api"
+import { useUserContext } from "../app/Provider"
+import '../css/styles.css'
 
 const ListadoDeEventos = () => {
+    const [eventos, setEventos] = useState([])
+    const [usuario] = useUserContext()
+
+    useEffect(() => {
+        getItems('events').then(data => {
+            const filteredEvents = data.filter(event => event.destinatarioId === usuario.uid);
+
+            const arrayIds = []
+            filteredEvents.map(events => arrayIds.push(events.remitenteId))
+            const filteredArrayIds = arrayIds.filter((arrayId, index) => arrayIds.indexOf(arrayId) === index)
+
+            getUsersByUserId(filteredArrayIds).then(data2 => {
+                const matchingThreadTexts = data.map(d => {
+                    const remitente = data2.find(d2 => d2.id === d.remitenteId);
+
+                    return remitente && d.destinatarioId === usuario.uid ? {
+                        msg: d.msg,
+                        userName: remitente.name,
+                        messageId: d.id,
+                        destinatarioId: d.destinatarioId
+                    } : null;
+                }).filter(o => o != null)
+
+                setEventos(matchingThreadTexts)
+            })
+
+        })
+    }, [])
+
     return (
         <>
-
-            <Link to={"/perfilusario"}><button>Perfil</button></Link>
+            <Link to={"/perfilusario"}> <button>Perfil</button></Link>
             <h1>Listado de eventos</h1>
+
+            {eventos &&
+                <div className="container">
+                    {eventos.map((evento, index) => {
+                        return (
+                            <div key={index} className="events">
+                                <h1>{evento.msg}</h1>
+                                <h3>Organizado por: {evento.userName}</h3>
+                            </div>
+                        );
+                    })}
+                </div>
+            }
+
             <Link to={"/nuevoevento"}>+</Link>
-            <h1>Registrate</h1>
-            <Link to={"/register"}><button>registrarse</button></Link>
-            <h1>Que voy a llevar</h1>
-            <Link to={"/eventcontribution"}><button>Que voy a llevar</button></Link>
+            <Link to={"/register"}><button>Register</button></Link>
+            <Link to={"/eventcontribution"}> <button>Que voy llevar</button></Link>
 
         </>
     )
